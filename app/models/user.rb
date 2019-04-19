d98484b8d485be3b1c7e4cf7ed7a8e86a61afb9f
kakimoso <k.i.moso9532@gmail.com>
2019-04-19 12:30:08 +0900

    init

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  extend Devise::Models
+  include DeviseTokenAuth::Concerns::User
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+end
